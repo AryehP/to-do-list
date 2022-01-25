62f2bb54ac2c43dc39d71e1a68635efdfde046c0
Aryeh Packer <arieh87@gmail.com>
2022-01-25 16:48:18 +0200

    firtst commit

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList items= {
+        [
+          {
+            id: 0,
+            text: "Task A",
+            complete: false,
+          },
+          {
+            id: 1,
+            text: "Task B",
+            complete: false,
+          },
+          {
+            id: 2,
+            text: "Task C",
+            complete: true,
+          },
+          {
+            id: 3,
+            text: "Task D",
+            complete: false,
+          },
+        ]
+      }/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItems.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodoItems() {
+  return <div></div>;
+}
+
+export default TodoItems;
